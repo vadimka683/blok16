ea9c249e0a6783640dd782b55e829d80ee595f42
vivasuk683 <vivas@DESKTOP-BE431TS>
2024-11-25 02:51:33 +0300

    задача 1, через поток

--- Задача1/Задача1/main1.cpp
+++ Задача1/Задача1/main1.cpp
@@ -5,41 +5,23 @@ using namespace std;
 
 
 int main() {
-	char now_speed[9] = "0 km/h";
+	string now_speed = "0 km/h";
+	stringstream speed(now_speed);
 	do {
 		float delta;
 		cout << "Input speed delta: ";
 		cin >> delta;
-		stringstream speed(now_speed);
 		float now_speed_f;
-		char measurements[4];
+		string measurements;
 		speed >> now_speed_f >> measurements;
+		stringstream().swap(speed);  // очистка потока через swap c пустым конструктором потока
 		if (now_speed_f + delta > 150) {
 			cout << "Inposible speed\n";
 			continue;
 		}
-		sprintf_s(now_speed, "%1.f", now_speed_f + delta);
-		if (now_speed_f + delta < 10) {
-			now_speed[1] += ' ';
-		}
-		else if (now_speed_f + delta < 100) {
-			now_speed[2] += ' ';
-		}
-		else {
-			now_speed[3] += ' ';
-		}
-		bool flag = false;
-		int count = 0;
-		for (int i = 0; i < sizeof(now_speed); i++) {
-			if (now_speed[i] == ' ' && flag == false) {
-				flag = true;
-				continue;
-			}
-			if (flag) {
-				now_speed[i] = measurements[count];
-				count++;
-			}
-		}
+		now_speed_f += delta;
+		speed << now_speed_f << measurements;
+		now_speed = speed.str();
 		cout << now_speed;
 		
 	} while (stof(now_speed) > 0.01);
